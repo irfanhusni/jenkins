65c4bc8f65227ac0e9f2959986bbde048d97dfa0
irfan husni alawi <irfan.husni@gmail.com>
2017-02-01 16:33:16 +0700

    add nuget

--- Jenkins/Program.cs
+++ Jenkins/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,8 @@ namespace Jenkins
     {
         static void Main(string[] args)
         {
-			// TO DO 
+            // TO DO 
+            IOrganizationService svc = null;
         }
     }
 }
